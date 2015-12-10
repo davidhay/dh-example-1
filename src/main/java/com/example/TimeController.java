59d110037e60e419c5cf337f6b21a21812ca9739
David Hay <david.j.hay@gmail.com>
2015-12-10 21:38:13 +0000

    Simple Spring Boot App

---
+++ src/main/java/com/example/TimeController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TimeController {
+	
+	@RequestMapping("/time")
+	@ResponseBody
+	public String showTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		String time = sdf.format(new Date());
+		
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("<meta http-equiv=\"refresh\" content=\"3\">");
+		String msg = String.format("The time is %s", time);
+		sb.append(msg);
+		return sb.toString();
+	}
+
+}
