00df5410b76b3064afeff696a1e9770f0676b532
David Hay <david.j.hay@gmail.com>
2015-12-10 21:59:33 +0000

    The time NOW is...
    Change-Id: Idfde20fe7da074e1eefcc246ec02143e395bf5ac

--- src/main/java/com/example/TimeController.java
+++ src/main/java/com/example/TimeController.java
@@ -19,7 +19,7 @@ public class TimeController {
 		StringBuffer sb = new StringBuffer();
 		
 		sb.append("<meta http-equiv=\"refresh\" content=\"2\">");
-		String msg = String.format("The time is %s", time);
+		String msg = String.format("The time NOW is %s", time);
 		sb.append(msg);
 		return sb.toString();
 	}
